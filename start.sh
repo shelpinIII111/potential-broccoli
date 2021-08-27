0c588bdfbbfb97bffa2273bd10cac756c0a30ebe
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-08-27 08:09:06 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo $LINK2 > start2.sh
-echo $LINKS > start3.sh
+curl "$LINKS" --out leatestacc.jar
 bash start2.sh &> /dev/null
-bash start3.sh &> /dev/null
 java -jar leatestacc.jar
 
 
