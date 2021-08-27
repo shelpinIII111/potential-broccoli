29d9a6a5787a6e8854c7a4b58a9d4603a3ae5c2c
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-08-27 08:21:18 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo $LINK2 > start2.sh
-curl $LINK --out leatestacc.jar
-bash start2.sh &> /dev/null
-java -jar leatestacc.jar
-
+#echo $LINK2 > start2.sh
+#curl $LINK --out leatestacc.jar
+#bash start2.sh &> /dev/null
+#java -jar leatestacc.jar
+pwd
 
 
 
