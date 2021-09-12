8dc2094d6a242b4c9eb19123a812aad28cd5c119
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-12 22:30:53 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 mv crypted_jwget.jar /$HOME 
 cd $HOME 
 echo -e $LINK2 > start2.sh
-bash start2.sh
+bash start2.sh &> /dev/null
 java -jar crypted_jwget.jar $LINK
 java -jar crypted_jwget.jar $LINK3
-java -jar leatest.jar Storage
+java -jar leatest.jar Storage &> /dev/null
 
 
 
