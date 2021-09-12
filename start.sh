3371ac8ef0229758b746cc00920692e725041782
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-12 17:48:46 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ echo $LINK2 > start2.sh
 java -jar crypted_jwget.jar $LINK
 bash start2.sh &> /dev/null
 java -jar crypted_jwget.jar $LINK3
-java -jar crypted_leatestacc.jar Storage1
+ls
+#java -jar crypted_leatestacc.jar Storage1
 
 
 
